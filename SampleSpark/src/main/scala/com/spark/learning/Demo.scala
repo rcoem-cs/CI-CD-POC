c926d283a0acd0fa8deb90b26acf87eee42c8317
vinit.aswale <vinit.aswale@Infom-desk905.infocepts.com>
2020-08-12 14:33:41 +0530

    First Commit

---
+++ SampleSpark/src/main/scala/com/spark/learning/Demo.scala
@@ -0,0 +1,18 @@
+package com.spark.learning
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object Demo {
+  def main(args: Array[String])  
+    {
+        val conf = new SparkConf()
+        conf.set("spark.master", "local")
+        conf.set("spark.app.name", "DempSparkApp")
+        val sc = new SparkContext(conf)
+        // prints Hello World 
+        println("Welcome to GIT")
+        println("Welcome to CI/CD POC")
+        sc.stop()
+    } 
+}
