4af8f9fa70652f9cb147d862126bb819c158de70
vinit.aswale <vinit.aswale@Infom-desk905.infocepts.com>
2020-08-27 15:45:58 +0530

    final commit

--- SampleSpark/src/test/wordcount/TestCases.scala
+++ SampleSpark/src/test/wordcount/TestCases.scala
@@ -5,10 +5,12 @@ import org.junit.Assert._
 class UnitTestCases {
   
   @Test def countWords {
+      println("Executed first test case")
       assertTrue(true)
   }
   
   @Test def checkTotal {
+      println("Executed second test case")
       assertTrue(true)
   }
   
