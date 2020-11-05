f011a0c507e949042156dadf333ae0b30e39bee8
Vinit S. Aswale <vinit.aswale@infocepts.com>
2020-11-05 18:00:32 +0530

    New QA code has been added

--- QATesting/src/main/scala/com/Infocepts/QATesting/sparkConf.scala
+++
@@ -1,7 +0,0 @@
-package com.Infocepts.QATesting
-
-import org.apache.spark.sql.SparkSession
-
-class sparkConf {
-  val spark = SparkSession.builder().master("local").appName("CICD Pipeline POC QA").config("hive.metastore.uris","thrift://im-hdp-mgr1.infocepts.com:9083").enableHiveSupport().getOrCreate()
-}
